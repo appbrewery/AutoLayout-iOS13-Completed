86dfd05f98e9df16a8b362f5fe93ec16e0464d8b
Angela <8798027+angelabauer@users.noreply.github.com>
2019-08-21 12:03:07 +0100

    Update Constraints

--- AutoLayout-iOS13/ViewController.swift
+++ AutoLayout-iOS13/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBAction func RollButtonPressed(_ sender: UIButton) {
+        print("pressed")
     }
-
-
+    
 }
 
