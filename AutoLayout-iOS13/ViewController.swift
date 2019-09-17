6e756eba2ceef8af396fa1361d1a22a6bf0bb74b
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-17 14:27:15 +0100

    Updated code

--- AutoLayout-iOS13/ViewController.swift
+++ AutoLayout-iOS13/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-    @IBAction func RollButtonPressed(_ sender: UIButton) {
-        print("pressed")
-    }
-    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+     
+     @IBAction func rollButtonPressed(_ sender: UIButton) {
+         
+         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+         
+         diceImageView1.image = diceArray[Int.random(in: 0...5)]
+         diceImageView2.image = diceArray[Int.random(in: 0...5)]
+         
+     }
 }
 
